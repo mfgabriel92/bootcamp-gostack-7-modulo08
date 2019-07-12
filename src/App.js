10576bad36ba8b1267bcc47263df261a9b774eba
Gabriel Monteiro Fernandes <mfgbriel92@gmail.com>
2019-07-12 16:14:44 -0300

    Módulo 08 - Hook useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => <h1>Hello World</h1>
+const App = () => {
+  const [tech, setTech] = useState(['ReactJS', 'React Native'])
+  const [newTech, setNewTech] = useState('')
+
+  const handleAdd = () => {
+    setTech([...tech, newTech])
+    setNewTech('')
+  }
+
+  const handleChange = ({ target }) => {
+    setNewTech(target.value)
+  }
+
+  return (
+    <ul>
+      {tech.map(t => (
+        <li key={t}>{t}</li>
+      ))}
+      <input type="text" value={newTech} onChange={handleChange} />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </ul>
+  )
+}
 
 export default App
